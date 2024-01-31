dbe5a280c06ac448c182d690451de0f1603630cf
Rafid46 <mostafarafid45@gmail.com>
2024-01-31 20:53:58 +0600

    project created

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Components/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div className="font-poppins">
+      <RouterProvider router={router} />
+    </div>
+  </React.StrictMode>
+);
